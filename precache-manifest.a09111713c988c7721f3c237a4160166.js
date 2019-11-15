9e3ef46d551d0f2a6d4c633d8c0a593a9cb1f31b
Mihail Mikov <m.mikoff@gmail.com>
2019-11-15 23:49:55 +0200

    added social links

---
+++ precache-manifest.a09111713c988c7721f3c237a4160166.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc93488fdcee099370cd8385faa12bee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bf46029c51b93ffe853",
+    "url": "/static/css/main.4a87fe0f.chunk.css"
+  },
+  {
+    "revision": "3380d1b813b4a5a53637",
+    "url": "/static/js/2.b825255c.chunk.js"
+  },
+  {
+    "revision": "4bf46029c51b93ffe853",
+    "url": "/static/js/main.0b3a12b2.chunk.js"
+  },
+  {
+    "revision": "ffd1b5a558070b7deb4a",
+    "url": "/static/js/runtime-main.3d1e2e47.js"
+  }
+]);
