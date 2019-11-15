6af2f8ff5b3034b308df1815adb2ec7c01ce52f8
Mihail Mikov <mihail.mikov@skyscanner.net>
2019-11-15 16:50:31 +0200

    reactified page

---
+++ precache-manifest.c30510c092c0f10418a2161049eb53ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8197a388d01020c1822c",
+    "url": "/static/js/main.c563c57b.chunk.js"
+  },
+  {
+    "revision": "407ab141deba78abe319",
+    "url": "/static/js/2.d042e1ad.chunk.js"
+  },
+  {
+    "revision": "8197a388d01020c1822c",
+    "url": "/static/css/main.56f20b94.chunk.css"
+  },
+  {
+    "revision": "697ce201dd4936262a97c450deb597fb",
+    "url": "/index.html"
+  }
+];
